70abff0bc4b625adaa0cf1ff83deea571f707807
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-01-24 15:06:58 +0100

    first

---
+++ testDockerCompose/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testDockerCompose.Models;
+
+namespace testDockerCompose.Data
+{
+   
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
---
+++ testDockerCompose/Extentions/MigrationExtentions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testDockerCompose.Data;
+
+namespace testDockerCompose.Extentions
+{
+    public static class MigrationExtentions 
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            
+            using AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+             dbContext.Database.Migrate();
+            
+        }
+    }
+}
